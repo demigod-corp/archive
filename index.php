5f7aa4ea25fcdd29220cafd9c1b9c3dd5f6f7fd6
demigod-corp <65177571+demigod-corp@users.noreply.github.com>
2020-05-11 08:55:21 -0600

    added files

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+<head><title></title></head>
+<body>
+
+<!-- replace "http://google.com" with the page you want it to redirect to. -->
+<!-- replace 'letmein' with the password you want -->
+
+	<?php
+	if (isset($_POST['password'])) {
+		if($_POST['password'] == 'letmein') {
+
+			header("Location: https://demigod-corp.github.com/influx/r9escAVjZUsiGpGJ.html");
+			exit;
+
+} else {
+
+		echo "password incorrect bitchboy";
+		exit;
+
+	}
+}
+?>
+
+<!-- replace "password.php" with the name of the password page. you can just leave the filename the way it is. -->
+
+<form action="index.php" method="POST">
+	Password: 
+	<br />
+	<input type="password" name="password" />
+	<input type="submit" value="Submit" />
+</form>
+
+</body>
+</html>
